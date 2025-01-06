37c8cf27efc1d854b6b2e408b001c529b9911e7d
tedspare <ted.spare@gmail.com>
2025-01-06 12:34:41 -0500

    Make points per invite a const

--- src/lib/utils/points.ts
+++ src/lib/utils/points.ts
@@ -1,7 +1,7 @@
 import type { Market, User } from '@prisma/client'
 
 const POINTS_PER_MARKET = 10
-
+const POINTS_PER_INVITE = 10
 export type UserWithPoints = User & {
 	points: number
 }
@@ -10,7 +10,7 @@ export function calculatePoints(
 	user: User & { invitees: { id: string }[] } & { markets: Market[] }
 ): number {
 	const marketPoints = user.markets.length * POINTS_PER_MARKET
-	const invitePoints = user.invitees.length * 10
+	const invitePoints = user.invitees.length * POINTS_PER_INVITE
 
 	const totalPoints = marketPoints + invitePoints
 
