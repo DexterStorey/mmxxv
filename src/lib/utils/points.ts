4642c1215f4ccfb3e5ae42f909be05b933d91cc2
tedspare <ted.spare@gmail.com>
2025-01-06 12:38:06 -0500

    Cap invite points to 100

--- src/lib/utils/points.ts
+++ src/lib/utils/points.ts
@@ -2,6 +2,8 @@ import type { Market, User } from '@prisma/client'
 
 const POINTS_PER_MARKET = 10
 const POINTS_PER_INVITE = 10
+const MAX_INVITE_POINTS = 100
+
 export type UserWithPoints = User & {
 	points: number
 }
@@ -10,7 +12,7 @@ export function calculatePoints(
 	user: User & { invitees: { id: string }[] } & { markets: Market[] }
 ): number {
 	const marketPoints = user.markets.length * POINTS_PER_MARKET
-	const invitePoints = user.invitees.length * POINTS_PER_INVITE
+	const invitePoints = Math.min(user.invitees.length * POINTS_PER_INVITE, MAX_INVITE_POINTS)
 
 	const totalPoints = marketPoints + invitePoints
 
